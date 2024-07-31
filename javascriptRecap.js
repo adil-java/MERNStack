bdd1ff1dca2cbf16767a79bbe8dfb6c527c2bea0
Adil Javed <adiljavaid125@gmail.com>
2024-07-31 21:44:56 +0500

    Add javascript file

---
+++ javascriptRecap.js
@@ -0,0 +1,20 @@
+// let n =10;
+// let arr = [1,2,3,4,5];
+// arr.forEach(e => {
+//     console.log(`${e} to the  power 2 is:${Math.pow(e,2)}`)
+// });
+// let min =10
+// let max = 20
+// let rand = Math.random()*(max-min)+min+1;
+// console.log(Math.floor(rand));
+// let arr =[1,1,1,2,2,3]
+// let k =1;
+// let st=new Set(arr);
+// let arrst = Array.from(st)
+// for(i=1;i<=k;i++){
+//     console.log(arrst[i-1])
+// }
+let date = new Date();
+// let hours = date.getHours(); // Get the current hour in 24-hour format
+// let temp = hours > 12 ? hours - 12 : hours; // Convert to 12-hour format
+console.log(date.toLocaleString());
