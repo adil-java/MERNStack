2e4e97624f9d98e2da9bfee3c8c2b9e10854ea54
Adil Javed <adiljavaid125@gmail.com>
2024-08-03 01:02:19 +0500

    project 1 in which click events are covered

--- Projectsjs/p1/main.js
+++ Projectsjs/p1/main.js
@@ -4,25 +4,43 @@ let black= document.getElementsByClassName('black')[0];
 let grey = document.getElementsByClassName('grey')[0];
 let reset = document.getElementsByClassName('reset')[0];
 let body = document.getElementsByTagName('body')[0];
-blue.addEventListener('click',()=>{
-    body.style.backgroundColor ='blue';
+let allcolor = document.querySelectorAll("div");
+allcolor.forEach(function(e) {
+    e.addEventListener('click', function() {
+        if (e.id===('blue')) {
+            body.style.backgroundColor = e.id;}
+            else if (e.id===('red')) {
+                body.style.backgroundColor = e.id;}
+                else if (e.id===('black')) {
+                    body.style.backgroundColor = e.id;}
+                    else if (e.id===('grey')) {
+                        body.style.backgroundColor = e.id;}
+                      else if(e.id==='white'){
+                        body.style.backgroundColor = e.id;
+                      }
+});
+    });   
+    console.log(allcolor)
+// });
+// blue.addEventListener('click',()=>{
+//     body.style.backgroundColor ='blue';
 
-})
-red.addEventListener('click',()=>{
-    body.style.backgroundColor ='red';
+// })
+// red.addEventListener('click',()=>{
+//     body.style.backgroundColor ='red';
 
-})
-red.addEventListener('click',()=>{
-    body.style.backgroundColor ='red';
+// })
+// red.addEventListener('click',()=>{
+//     body.style.backgroundColor ='red';
 
-})
-black.addEventListener('click',()=>{
-    body.style.backgroundColor ='black';
+// })
+// black.addEventListener('click',()=>{
+//     body.style.backgroundColor ='black';
 
-})
-grey.addEventListener('click',()=>{
-    body.style.backgroundColor ='grey';
+// })
+// grey.addEventListener('click',()=>{
+//     body.style.backgroundColor ='grey';
 
-})
-reset.addEventListener('click',()=>{body.style.backgroundColor='';})
+// })
+
 
