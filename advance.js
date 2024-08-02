19ad540eea3a203d2780cbc32f191fd0eb0d374c
Adil Javed <adiljavaid125@gmail.com>
2024-08-02 20:42:40 +0500

    optimizing code

--- advance.js
+++ advance.js
@@ -1,18 +1,24 @@
 let increase = document.getElementById('inc');
 let decrease =document.getElementById('dec');
 let count= 0;
+let main = document.getElementById('main');
+let msg = document.createElement('div');
+msg.id = "val";
+let p = document.createElement('p');
+main.appendChild(msg);
+msg.appendChild(p)
 
 
     increase.addEventListener('click',()=>{
         count++;
-        document.getElementById('val').innerHTML=`<p>current value is: ${count}</p>`
+        p.innerHTML=`<p>current value is: ${count}</p>`
         
     })
     
     decrease.addEventListener('click',()=>{
         if (count > 0) {
             count--;
-            document.getElementById('val').innerHTML = `<p>current value is: ${count}</p>`;
+            p.innerHTML = `<p>current value is: ${count}</p>`;
         }
         
             
