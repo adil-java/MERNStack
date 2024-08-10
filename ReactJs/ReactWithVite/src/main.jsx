7b896205d3c0da449eb3a4430f0f4cff0da761b2
Adil Javed <adiljavaid125@gmail.com>
2024-08-10 23:48:26 +0500

    Learn: How things works under the hood

--- ReactJs/ReactWithVite/src/main.jsx
+++ ReactJs/ReactWithVite/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Main from './mypage.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(// react create it own DOM and then compare it with the Page Dom, and merges the change part only.
   <StrictMode>
     <App />
     <Main/>
