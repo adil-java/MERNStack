885791adc5e29f57ba810b41646b4bca06488095
Adil Javed <adiljavaid125@gmail.com>
2024-08-11 23:59:11 +0500

    H

--- ReactJs/ReactProject/P1/MyCounter/src/App.jsx
+++ ReactJs/ReactProject/P1/MyCounter/src/App.jsx
@@ -1,39 +1,25 @@
-import React  from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import {useState,useEffect} from "react";
+import { createRoot } from "react-dom/client";
 
-import { useState } from 'react';
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+function Tick() {
+  let [time,setTime]= useState(new Date().toLocaleTimeString())    
+  useEffect(()=>{
+    let timer= setInterval(()=>{
+      setTime(new Date().toLocaleTimeString())},1000);
 
-
-function App() {
-  let [counter,setcounter]=useState(0);
-  const inc=()=>{
-    if(counter<20){
-
-      setcounter(++counter);
-      
-      console.log(counter);
-    }
-  }
-  const dec=()=>{
-    if(counter>0){
-
-      setcounter(counter-1);
-      
-      console.log(counter);
-    }
+      return()=>clearInterval(timer)
+  },[]);
   
-  }
-  return (
-    <>
-     <h1>Code With Adil</h1>
-     <h2 >Counter Value:{counter}</h2>
-     <button 
-     onClick={inc}>increase</button>
-     <button onClick={dec}>Decrese</button>
-    </>
-  )
+return(
+
+  <div>
+     <h1>Hello, world!</h1>
+     <h2>It is {time}.</h2>
+   </div>
+)
 }
 
-export default App
+
+export default Tick;
