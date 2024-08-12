5dd906dd7fcd475b1148a042dc21ca34f0ca67b8
Adil Javed <adiljavaid125@gmail.com>
2024-08-13 01:32:26 +0500

    Interview question in Counter

--- ReactJs/ReactProject/P1/MyCounter/src/App.jsx
+++ ReactJs/ReactProject/P1/MyCounter/src/App.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {useState,useEffect} from "react";
 import { createRoot } from "react-dom/client";
-
 function Tick() {
+  
+  const [counter,setCounter]=useState(0)
+  function inc(){
+    setCounter(counter+1)
+    // setCounter(counter+1) // In fiber algo it create batches of changes to all the setCounter() is treated as one to fullfill this condition we can use the below way
+    // setCounter(pervCounter=>pervCounter+1)// on the increase button incr counter twice 
+
+  }
+  function dec(){
+    setCounter(counter-1)
+  }
   let [time,setTime]= useState(new Date().toLocaleTimeString())    
   useEffect(()=>{
     let timer= setInterval(()=>{
@@ -17,6 +27,10 @@ return(
   <div>
      <h1>Hello, world!</h1>
      <h2>It is {time}.</h2>
+     <h1>The counter is:{counter}</h1>
+     <button onClick={dec}>decrease</button>
+     <button onClick={inc}>inrease</button>
+
    </div>
 )
 }
