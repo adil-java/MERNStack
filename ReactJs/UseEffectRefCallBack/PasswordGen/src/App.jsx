740b7645e4d847f94f4d800fd03ff7700d2d08e5
Adil Javed <adiljavaid125@gmail.com>
2024-08-16 02:32:27 +0500

    initialized the New React Project

---
+++ ReactJs/UseEffectRefCallBack/PasswordGen/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+  return (
+    
+    <div className='bg-black text-yellow-400 h-screen w-full'
+    >
+
+      <h1 className='text-yellow-400'>Hello</h1>
+    </div>
+    
+  )
+}
+
+export default App
